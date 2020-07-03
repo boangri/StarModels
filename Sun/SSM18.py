9b663fda052fa5a84144a46f2475edcbe565bba9
boris@acer <xinu@yandex.ru>
2020-07-03 22:51:31 +0300

    Added interpolation of model data (18 -> 1021 with K=60)

--- Sun/SSM18.py
+++ Sun/SSM18.py
@@ -27,23 +27,101 @@ X18 = [.355, .417, .497, .592, .641, .668, .688, .702, .716, .724, .728, .731, .
 K18 = [1.1, 1.2, 1.3, 1.4, 1.6, 1.7, 1.8, 1.9, 2.1, 2.4, 2.8, 3.5, 4.7, 8.0, 12.2, 16.8, 10, 0.3]
 
 # NumPy arrays
-m = np.array(M18)
-r = np.array(R18)
-t = 1e6 * np.array(T18)
-d = np.array(D18)
-l = np.array(L18)
-x = np.array(X18)
-k = np.array(K18)
-c = np.zeros(18)
-c[16] = 1.
+m1 = np.array(M18)
+r1 = np.array(R18)
+t1 = 1e6 * np.array(T18)
+d1 = np.array(D18)
+l1 = np.array(L18)
+x1 = np.array(X18)
+o1 = np.array(K18)
+c1 = np.zeros(18)
+c1[16] = 1.
 
 # Pressure from density and temperature
-p = d * t * (2 * x + .75 * (1 - Z - x) + .5 * Z) * ph.kB / ph.m_prot
+p1 = d1 * t1 * (2 * x1 + .75 * (1 - Z - x1) + .5 * Z) * ph.kB / ph.m_prot
 
 
 def load_data():
+    return pd.DataFrame({'Mass': m1, 'Radius': r1, 'Temperature': t1, 'Density': d1, 'Luminosity': l1,
+                         'Hydrogen': x1, 'Opacity': o1, 'Convection': c1, 'Pressure': p1})
+
+
+"""
+Data interpolation
+"""
+
+
+def interpolate(x, x0, x1, x2, y0, y1, y2):
+    """
+    Interpolation with 2nd degree polynomial.
+    x must be between x0 and x2
+    """
+    u1 = x1 - x0
+    u2 = x2 - x0
+    w1 = y1 - y0
+    w2 = y2 - y0
+    a = (w1 * u2 - w2 * u1) / (u1 * u1 * u2 - u2 * u2 * u1)
+    b = (w1 * u2 * u2 - w2 * u1 * u1) / (u1 * u2 * u2 - u2 * u1 * u1)
+    u = x - x0
+    y = y0 + (a * u + b) * u
+    return y
+
+
+def load_interpolated_data(K=12):
+    """
+    :param K: Number of intermediate (interpolated) layers
+    :return: DataFrame 17*K+1 rows, 8 columns
+    """
+    N = 17  # number of original layers
+
+    r = np.zeros(N * K + 1)
+    m = np.zeros(N * K + 1)
+    t = np.zeros(N * K + 1)
+    d = np.zeros(N * K + 1)
+    l = np.zeros(N * K + 1)
+    x = np.zeros(N * K + 1)
+    o = np.zeros(N * K + 1)
+    p = np.zeros(N * K + 1)
+    r[N * K] = r1[N]
+    m[N * K] = m1[N]
+    t[N * K] = t1[N]
+    d[N * K] = d1[N]
+    l[N * K] = l1[N]
+    x[N * K] = x1[N]
+    o[N * K] = o1[N]
+
+    for i in range(N - 1):
+        for j in range(K):
+            R = r1[i] + j * (r1[i + 1] - r1[i]) / K
+            r[j + K * i] = R
+            m[j + K * i] = interpolate(R, r1[i], r1[i + 1], r1[i + 2], m1[i], m1[i + 1], m1[i + 2])
+            t[j + K * i] = interpolate(R, r1[i], r1[i + 1], r1[i + 2], t1[i], t1[i + 1], t1[i + 2])
+            d[j + K * i] = interpolate(R, r1[i], r1[i + 1], r1[i + 2], d1[i], d1[i + 1], d1[i + 2])
+            l[j + K * i] = interpolate(R, r1[i], r1[i + 1], r1[i + 2], l1[i], l1[i + 1], l1[i + 2])
+            x[j + K * i] = interpolate(R, r1[i], r1[i + 1], r1[i + 2], x1[i], x1[i + 1], x1[i + 2])
+            o[j + K * i] = interpolate(R, r1[i], r1[i + 1], r1[i + 2], o1[i], o1[i + 1], o1[i + 2])
+
+    for j in range(K):
+        m[j] = m1[1]*pow(r[j]/r1[1], 3)
+        l[j] = l1[1]*pow(r[j]/r1[1], 3)
+        # m[j] = m[j-1] + d[j]*4/3*ph.pi*(pow(r[j], 3) - pow(r[j-1], 3))
+    for j in range(K):
+        R = r1[N - 1] + (r1[N] - r1[N - 1]) * j / K
+        r[j + K * (N - 1)] = R
+        m[j + K * (N - 1)] = interpolate(R, r1[N - 2], r1[N - 1], r1[N], m1[N - 2], m1[N - 1], m1[N])
+        t[j + K * (N - 1)] = interpolate(R, r1[N - 2], r1[N - 1], r1[N], t1[N - 2], t1[N - 1], t1[N])
+        d[j + K * (N - 1)] = interpolate(R, r1[N - 2], r1[N - 1], r1[N], d1[N - 2], d1[N - 1], d1[N])
+        l[j + K * (N - 1)] = interpolate(R, r1[N - 2], r1[N - 1], r1[N], l1[N - 2], l1[N - 1], l1[N])
+        x[j + K * (N - 1)] = interpolate(R, r1[N - 2], r1[N - 1], r1[N], x1[N - 2], x1[N - 1], x1[N])
+        o[j + K * (N - 1)] = interpolate(R, r1[N - 2], r1[N - 1], r1[N], o1[N - 2], o1[N - 1], o1[N])
+
+    for j in range(N * K + 1):
+        if m[j] > 1.:
+            m[j] = 1.
+        p[j] = ph.Pressure(d[j], t[j], x[j], 1 - Z - x[j], Z)
+
     return pd.DataFrame({'Mass': m, 'Radius': r, 'Temperature': t, 'Density': d, 'Luminosity': l,
-                         'Hydrogen': x, 'Opacity': k, 'Convection': c, 'Pressure': p})
+                         'Hydrogen': x, 'Opacity': o, 'Pressure': p})
 
 
-print('SSM18 version 1.1')
+print('SSM18 version 1.4')
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Sun import SSM18
 print('speed of light = %e' % ph.c)
 print('Solar mass =  %e' % SSM18.M)
 
-df = SSM18.load_data()
+df = SSM18.load_interpolated_data()
 
 print(df)
 print(ph.Pressure(100, 13e7, 0.732, 1, 0.02))
